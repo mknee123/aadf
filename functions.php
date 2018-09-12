85bf7b1dfe9950d6475461802bbdc8b3acebb3ae
MurKat <mknee123@gmail.com>
2018-09-12 14:55:41 -0400

    Almost complete gulpfile.js. Add update to functions file to point to minified js file. Added .jshintrc config file

--- functions.php
+++ functions.php
@@ -121,10 +121,10 @@ add_action( 'widgets_init', 'aadf_widgets_init' );
  */
 function aadf_scripts() {
 	wp_enqueue_style( 'aadf-style', get_stylesheet_uri() );
+	wp_enqueue_script( 'gulp-js', get_template_directory_uri() . '/js/app.min.js', array(), '20151215', true )
+	//wp_enqueue_script( 'aadf-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'aadf-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'aadf-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'aadf-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -158,4 +158,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,82 @@
 /**
 Required Files
 */
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    cleanCSS = require('gulp-clean-css');
+require('es6-promise').polyfill();
+var gulp = require('gulp');
+    autoprefixer  = require('gulp-autoprefixer');
+
+    uglify = require('gulp-uglify');
+    concat = require('gulp-concat');
     jshint = require('gulp-jshint');
+
+
+    plumber = require('gulp-plumber');
+    gutil = require('gulp-util');
+    cleanCSS = require('gulp-clean-css');
+
+    imagemin = require('gulp-imagemin');
+
     rename = require('gulp-rename');
 
 /**Development Server **/
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('browser-sync', function() {
+
+var onError = function( err ) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
+
+
+//CSS
+gulp.task('css', function() {
+  return gulp.src('./styles/**/*.css')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(cleanCSS())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
+
+});
+
+//JS
+gulp.task('js', function() {
+  return gulp.src(['./js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('app.js'))
+  .pipe(rename({suffix:'.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('./js'))
+});
+
+//IMAGES
+gulp.task('images', function() {
+  return gulp.src('./images/src/*')
+  .pipe(plumber({errorHandler: onError}))
+  .pipe(imagemin({optimizationLevel: 7, progress: true}))
+  .pipe(gulp.dest('./images/use'));
+})
+
+
+// WATCH
+gulp.task('watch', function() {
   browserSync.init({
+    files: ['./**/*.php'],
     proxy: 'http://localhost',
   });
+  gulp.watch('./styles/**/*.css', ['css', reload ]);
+  gulp.watch('./js/*.js', ['js', reload ]);
+  gulp.watch('./images/src/*', ['images', reload ]);
 });
+
+// default task
+//gulp.task('default', function(working) {
+//  console.log('hello');
+//  working();
+
+//});
+
+
+gulp.task('default', ['css', 'js', 'images', 'watch']);
