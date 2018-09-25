f82f9c2b5672dbfa30585f79cc08e811a6117376
MurKat <mknee123@gmail.com>
2018-09-25 15:38:38 -0400

    Added aadf blue color to all links, including the subpage footer links. Applied and orange color when those links have a hover state

--- header-home.php
+++ header-home.php
@@ -17,7 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<script src="https://unpkg.com/scrollreveal"></script>
-	
+
 	<?php wp_head(); ?>
 </head>
 
